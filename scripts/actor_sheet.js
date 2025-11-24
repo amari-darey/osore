78d389da83f1aafaf4d4d2e98d1af84740372858
Amari <filin1232@gmail.com>
2025-11-24 21:01:58 +0300

    char change

--- scripts/actor_sheet.js
+++ scripts/actor_sheet.js
@@ -10,4 +10,36 @@ export default class OsoreActorSheet extends ActorSheet {
       tabs: [{ nav: ".sheet-tabs", content: ".sheet-body", initial: "main" }]
     });
   }
+
+  getData(options = {}) {
+    const data = super.getData(options);
+
+    const currentChar = this.actor.getFlag("osore", "currentChar") || 1;
+
+    const activePath = `system.character_${currentChar}`;
+
+    const current = foundry.utils.getProperty(this.actor.system, `character_${currentChar}`) || {};
+
+    data.currentChar = currentChar;
+    data.activePath = activePath;
+    data.current = current;
+    data.isActive1 = currentChar === 1;
+    data.isActive2 = currentChar === 2;
+    data.isActive3 = currentChar === 3;
+    data.isActive4 = currentChar === 4;
+
+    return data;
+  }
+
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    html.find(".character-switcher .char-btn").click(async ev => {
+      ev.preventDefault();
+      const newChar = Number(ev.currentTarget.dataset.character);
+      if (!newChar || newChar < 1 || newChar > 4) return;
+      await this.actor.setFlag("osore", "currentChar", newChar);
+      this.render(true);
+    });
+  }
 }
